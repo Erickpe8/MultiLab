9e4aed98d978d39814ddae1573fae52a03ebc974
DavidAceros <136133547+Aceros113@users.noreply.github.com>
2026-01-18 22:13:38 -0500

    Refactor: Add call Seeders (Unit, Category, Brand, Location, DeviceModel and Material)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            CategorySeeder::class,
+            UnitSeeder::class,
+            BrandSeeder::class,
+            LocationSeeder::class,
+            DeviceModelSeeder::class,
+            MaterialSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
             ClassroomWorkstationSeeder::class,
