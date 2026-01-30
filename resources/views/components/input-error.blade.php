523ddff0adf8c14e6e278f89861352238ad27226
Erickpe8 <ericksperezc@gmail.com>
2026-01-30 15:20:25 -0500

    Actualizar los estilos del componente de error de entrada para mejorar la legibilidad y consistencia visual.

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'mt-2 text-sm leading-relaxed font-semibold text-rose-700 dark:text-rose-300 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'mt-2 space-y-1 text-sm leading-relaxed !font-bold !text-rose-600 dark:!text-rose-300']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
